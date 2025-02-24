4cfc3e84f12c7b3ac520cae63d1eb2c16bb1839b
João Vicente <joaovsilva70@outlook.come>
2025-02-24 16:52:45 -0300

    feat: shadcn ui initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+
+const ProductPage = () => {
+  return (
+    <div>
+      <Button>FSW 7</Button>
+    </div>
+  )
+}
+
+export default ProductPage
